54ba11aab07acb90c05ab76bb71e9f9804a65d28
Tejas Parmar <153416264+tejas9440@users.noreply.github.com>
2024-04-23 17:58:17 +0530

    Update userController.js

--- chocolate/backend/user/userController.js
+++ chocolate/backend/user/userController.js
@@ -231,8 +231,8 @@ module.exports.checkout = async (req, res) => {
         payment_method_types: ["card"],
         line_items: lineItems,
         mode: "payment",
-        success_url: "https://chocolate-3.onrender.com/success",
-        cancel_url: "https://chocolate-3.onrender.com/cancel",
+        success_url: "/success",
+        cancel_url: "/cancel",
         customer_email: email,
         billing_address_collection: 'required'
     };
